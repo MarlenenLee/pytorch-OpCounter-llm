54aafb03e51c3c76854378d87ce7acc8828a5329
MarleneLee <marlene_bj@163.com>
2025-02-08 17:57:26 +0800

    update peak memory function

--- demo_test.py
+++ demo_test.py
@@ -12,23 +12,24 @@ from models.llama import (
 
 class ModelArgs:
     dim: int = 8192                  # 8192
-    n_layers: int = 1                # 80
+    n_layers: int = 3                # 80
     n_heads: int = 64                # 64
     n_kv_heads: int = 8              # 8
-    vocab_size: int = 32000
+    vocab_size: int = 32256          # 32000
     #multiple_of: int = 4096
     #ffn_dim_multiplier: float = 1.3
     intermediate_size: int = 28672   # 28672 
     norm_eps: float = 1e-5
-    max_batch_size: int = 32
-    max_seq_len: int = 4096
+    max_batch_size: int = 1
+    max_seq_len: int = 2048
 
     model_parallel_size: int = 4
 
-    batch_size: int = 1
-    seq_len: int = 4096
+    batch_size: int = 8
+    seq_len: int = 2048
 
     use_amp: bool = True
+    reduce_bucket_size = 500000000
 
 
 
@@ -37,7 +38,7 @@ if __name__ == "__main__":
     batch_size = args.batch_size
     seq_len = args.seq_len
     device = torch.device('cuda:0')
-
+    """
     tokens = torch.randint(low=3,high=args.vocab_size,size=(batch_size,seq_len))
     tokens = tokens.to(device)
 
@@ -53,3 +54,27 @@ if __name__ == "__main__":
     )
     print("\nTotal FLOPs:", clever_format(macs), 
             "\tTotal Params:", clever_format(params))
+    """
+    total_params = 2 * args.vocab_size / args.model_parallel_size * args.dim + \
+                 args.n_layers * (2 * args.dim * args.dim / args.model_parallel_size + \
+                 2 * args.dim * args.dim * args.n_kv_heads / args.n_heads / args.model_parallel_size + \
+                 3 * args.dim * args.intermediate_size / args.model_parallel_size + 2 * args.dim) + args.dim
+
+    # params_without_norm_weight:
+    _params = total_params - 2 * args.n_layers * args.dim - args.dim
+
+    optim_peak_mem = 2 * total_params + 18 * _params + \
+            2 * args.batch_size * args.n_heads / args.model_parallel_size * args.seq_len * args.seq_len + \
+            4 * args.batch_size * args.seq_len * args.vocab_size / args.model_parallel_size
+
+    peak_mem = 6 * _params + 4 * args.batch_size * args.seq_len * args.dim + \
+            2 * args.batch_size * args.n_heads / args.model_parallel_size * args.seq_len * args.seq_len + \
+            args.n_layers * (10 * args.batch_size * args.seq_len * args.dim + \
+            8 * args.batch_size * args.seq_len * args.n_heads / args.model_parallel_size * (args.dim / args.n_heads) + \
+            2 * args.batch_size * args.n_heads / args.model_parallel_size * args.seq_len * args.seq_len + \
+            8 * args.batch_size * args.seq_len * args.intermediate_size / args.model_parallel_size) + \
+            10 * args.batch_size * args.seq_len * args.vocab_size / args.model_parallel_size + 2 * args.reduce_bucket_size
+
+    print("params:", total_params)
+    print("Optim Step Peak Memory:", optim_peak_mem, "=>", clever_format(optim_peak_mem))
+    print("BWD Peak Memory:", peak_mem, "=>", clever_format(peak_mem))
